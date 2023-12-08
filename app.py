02f687b56eef2675cb13af41ddb5a05061c42f5e
JustinPeach25 <justinpeach25@gmail.com>
2023-12-08 15:59:09 +0200

    Added routes to all pages

--- app.py
+++ app.py
@@ -1,10 +1,37 @@
-from flask import Flask
+from flask import Flask, url_for, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    return render_template('home.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+
+@app.route('/addnewemployee')
+def addnewemployee():
+    return render_template('addnewemployee.html')
+
+@app.route('/singleemployeeprofile')
+def singleemployeeprofile():
+    return render_template('singleemployeeprofile.html')
+
+@app.route('/updateemployee')
+def updateemployee():
+    return render_template('updateemployee.html')
+
+def logout():
+    render_template('home.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
