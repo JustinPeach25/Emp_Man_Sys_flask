be7a588f40160b3ccc75270e2c87078832f9d44f
JustinPeach25 <justinpeach25@gmail.com>
2023-12-09 18:34:19 +0200

    Created db file

--- schema.sql
+++ schema.sql
@@ -1,8 +1,3 @@
-create table employees(id integer AUTOINCREMENT PRIMARY key, name text not null, surname text not null, password text not null, admin bool not null default 'O')
+create table admin(id integer AUTO_INCREMENT PRIMARY key, name text not null, surname text not null, password text not null, admin bool not null default 'O');
 
-create table employees(empid integer AUTOINCREMENT PRIMARY key, name text not null, surname text not null, DOB date, salary float not null, position text not null, line_manager text not null);
-
-
-
-"surname, birth date, employee number, salary,
-role/position, and reporting line manager."
+create table employees(empid integer AUTO_INCREMENT PRIMARY key, name text not null, surname text not null, DOB date, salary float not null, position text not null, line_manager text not null);
